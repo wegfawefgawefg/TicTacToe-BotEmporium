7767f79f85e8615e98105a0f355f89640533d9da
wegfawefgawefg <668es218pur@gmail.com>
2020-04-04 10:42:48 -0700

    finish playng w/ nn ttt. probably final comit

--- nn2.py
+++ nn2.py
@@ -8,6 +8,17 @@ import tictactoe as tt
 from torch.autograd import Variable
 import random
 
+
+'''
+///LOOKS ABOUT DONE///
+///MOVING ON TO NEXT PROJECT///
+
+wins, losses, ties:
+899
+87
+14
+'''
+
 '''
 TODO:
 -make offline mode
@@ -15,7 +26,19 @@ TODO:
 -assert exploration is happening in play against self
 -why are moves worse without unenforcing invalid moves?, does it understand the game less? or is it just more training sets
 -expand the newtork, add dropout, try resnet
+-output precent chance of winning?
+-experiment with more inputs? such as whos turn it is?
+-investigate network dying: (SOLVED) (IF THE WEIGHTS ARE NEGATIVE, THE MAX WILL BE AN INVALID MOVE) SOLUTION: set masked vals to -inf instead of 0
+    -sometimes it learns a really poor meta and loses most of its games
+    -not sure whats going on here, i think some moves get so dissincentivised that they die
+-make it play against all random moves instead of just some random moves
+-make it occasionally make random moves to be more explorative or something
+
+
+TODO MAYBE:
 -gpu? batch training many games at once? (is this worth the effort)
+-tournament with other algorithms?
+-refactor all the tic tac toe game code to shrink it down?
 '''
 
 '''
@@ -34,12 +57,60 @@ class Net(nn.Module):
         self.outputSize = outputSize
         self.l1 = nn.Linear(inputSize, hiddenSize)
         self.l2 = nn.Linear(hiddenSize, hiddenSize)
-        self.l3 = nn.Linear(hiddenSize, outputSize)
+        self.l8 = nn.Linear(hiddenSize, outputSize)
+
+        # self.dropout1 = nn.Dropout(0.5)
+        # self.dropout2 = nn.Dropout(0.5)
 
     def forward(self, x):
-        x = F.relu( self.l1(x) )
-        x = F.relu( self.l2(x) )
-        x = self.l3(x)
+        x = F.leaky_relu( self.l1(x) )
+        # x = self.dropout1(x)
+        x = F.leaky_relu( self.l2(x) )
+        # x = self.dropout2(x)
+        x = self.l8(x)
+        return x
+        # return F.softmax(x, dim=2)
+
+class BigNet(nn.Module):
+    def __init__(self, inputSize, hiddenSize, outputSize):
+        super().__init__()
+        self.inputSize = inputSize
+        self.hiddenSize = hiddenSize
+        self.outputSize = outputSize
+        self.l1 = nn.Linear(inputSize, hiddenSize)
+        self.l2 = nn.Linear(hiddenSize, hiddenSize)
+        self.l3 = nn.Linear(hiddenSize, hiddenSize)
+        self.l4 = nn.Linear(hiddenSize, hiddenSize)
+        self.l5 = nn.Linear(hiddenSize, hiddenSize)
+        self.l6 = nn.Linear(hiddenSize, hiddenSize)
+        self.l7 = nn.Linear(hiddenSize, hiddenSize)
+        self.l8 = nn.Linear(hiddenSize, outputSize)
+
+        self.dropout1 = nn.Dropout(0.5)
+        self.dropout2 = nn.Dropout(0.5)
+        self.dropout3 = nn.Dropout(0.5)
+        self.dropout4 = nn.Dropout(0.5)
+        self.dropout5 = nn.Dropout(0.5)
+        self.dropout6 = nn.Dropout(0.5)
+        self.dropout7 = nn.Dropout(0.5)
+
+
+    def forward(self, x):
+        x = F.leaky_relu( self.l1(x) )
+        x = self.dropout1(x)
+        x = F.leaky_relu( self.l2(x) )
+        x = self.dropout2(x)
+        x = F.leaky_relu( self.l3(x) )
+        x = self.dropout3(x)
+        x = F.leaky_relu( self.l4(x) )
+        x = self.dropout4(x)
+        x = F.leaky_relu( self.l5(x) )
+        x = self.dropout5(x)
+        x = F.leaky_relu( self.l6(x) )
+        x = self.dropout6(x)
+        x = F.leaky_relu( self.l7(x) )
+        x = self.dropout7(x)
+        x = self.l8(x)
         return x
         # return F.softmax(x, dim=2)
 
@@ -52,11 +123,14 @@ def genBoard():
 
 def scoreEndBoard(board, winner, myPlayer):
     if not winner:
+        # return -1
         return 1
     elif winner == tt.togglePlayer(myPlayer):
-        return -10
+        return -1
+        # return -2
     elif winner == myPlayer:
-        return 10
+        return 5
+        # return 1
 
 '''
 should it know whos turn it is?
@@ -71,7 +145,8 @@ def oneHotTicTacToe(board, computersPlayer):
     return oneHot
 
 def trainAgainstSelf(net, criterion, optimizer, epochs):
-    numInvalidMoves = 0
+    net.train()
+
     for i in tqdm(range(epochs)):
         player = 2
         computersPlayer = random.randint(1,2)
@@ -89,44 +164,36 @@ def trainAgainstSelf(net, criterion, optimizer, epochs):
         movesB = []
         outputsB = []
         while(not winner and movesLeft):
-            move = None
-            moveValid = False
-            while not moveValid:
-                #   generate a move
-                if player == computersPlayer:
-                    oneHot = oneHotTicTacToe(board, computersPlayer).view(1, 1, 18)
-                else:
-                    oneHot = oneHotTicTacToe(board, tt.togglePlayer(computersPlayer)).view(1, 1, 18)
-                output = net(oneHot)
-                values, index = output.view(9).max(0)
-                if board.flatten()[index] == 0: #   if move is valid
-                    moveValid = True
+            #   generate a move
+            if player == computersPlayer:
+                oneHot = oneHotTicTacToe(board, computersPlayer).view(1, 1, 18)
+            else:
+                oneHot = oneHotTicTacToe(board, tt.togglePlayer(computersPlayer)).view(1, 1, 18)
+            output = net(oneHot)
 
-                    #   apply the move
-                    move = index
-                    board = board.flatten()
-                    if player == computersPlayer:
-                        board[move] = computersPlayer
-                    else:
-                        board[move] = tt.togglePlayer(computersPlayer)
-                    board = board.reshape(3, 3)
-                    
-                    #   store for later
-                    if player == computersPlayer:
-                        movesA.append(move)
-                        outputsA.append(output)
-                    else:
-                        movesB.append(move)
-                        outputsB.append(output)
-                else:   #   invalid move, prime the whip
-                    # print("invalid move")
-                    numInvalidMoves += 1
-                    optimizer.zero_grad()
-                    validMoves = np.where(board == 0, 1, 0)
-                    target = torch.tensor(validMoves, dtype=torch.float).view(1, 1, 9)
-                    loss = criterion(output, target)
-                    loss.backward()
-                    optimizer.step()
+            #   mask out invalid moves
+            invalidMoves = np.where( board.flatten() > 0, True, False)
+            maskedOutput = output.clone().view(9)
+            maskedOutput[invalidMoves] = -10
+            values, index = maskedOutput.max(0)
+
+            #   apply the move
+            move = index
+            board = board.flatten()
+            if player == computersPlayer:
+                board[move] = computersPlayer
+            else:
+                board[move] = tt.togglePlayer(computersPlayer)            
+            board = board.reshape(3, 3)
+
+            #   store for later
+            if player == computersPlayer:
+                movesA.append(move)
+                outputsA.append(output)
+            else:
+                movesB.append(move)
+                outputsB.append(output)
+
             player = tt.togglePlayer(player)
 
             movesLeft = np.any(np.where(board == 0, 1, 0))
@@ -159,6 +226,8 @@ def trainAgainstSelf(net, criterion, optimizer, epochs):
             optimizer.step()
 
 def train(net, criterion, optimizer, epochs):
+    net.train()
+
     for i in tqdm(range(epochs)):
         player = 2
         computersPlayer = random.randint(1,2)
@@ -169,6 +238,8 @@ def train(net, criterion, optimizer, epochs):
 
         movesLeft = np.any(np.where(board == 0, 1, 0))
         winner = tt.getWinner(board)
+
+        gameDuration = 0
 
         moves = []
         outputs = []
@@ -181,7 +252,7 @@ def train(net, criterion, optimizer, epochs):
                 #   mask out invalid moves
                 invalidMoves = np.where( board.flatten() > 0, True, False)
                 maskedOutput = output.clone().view(9)
-                maskedOutput[invalidMoves] = 0
+                maskedOutput[invalidMoves] = -10
                 values, index = maskedOutput.max(0)
 
                 #   apply the move
@@ -199,16 +270,22 @@ def train(net, criterion, optimizer, epochs):
                 randomMove = random.choice(empties)
                 tt.applyMove(player, randomMove, board)
             player = tt.togglePlayer(player)
+            gameDuration += 1
 
             movesLeft = np.any(np.where(board == 0, 1, 0))
             winner = tt.getWinner(board)
         
         #   get end score of game
         score = scoreEndBoard(board, winner, computersPlayer)
-        for i, move in enumerate(moves):
+        # gameDurationMultiplier = 1.0 - gameDuration / 10
+        # gameDurationMultiplier = gameDurationMultiplier * 0.9
+        dilutionFactor = 0.9
+        totalDilutant = 1.0
+        for i, move in reversed(list(enumerate(moves))):
+            totalDilutant *= dilutionFactor
             output = outputs[i]
             target = output.clone().view(9)
-            target[move] = score
+            target[move] = score * totalDilutant
             target = target.view(1, 1, 9)
 
             optimizer.zero_grad()
@@ -216,12 +293,12 @@ def train(net, criterion, optimizer, epochs):
             loss.backward()
             optimizer.step()
 
-def test(net, criterion, optimizer, epochs):
+def test(net, epochs):
+    net.eval()
+
     numWins = 0
     numLosses = 0
     numTies = 0
-
-    optimizer.zero_grad()
 
     for i in tqdm(range(epochs)):
         player = 2
@@ -242,7 +319,7 @@ def test(net, criterion, optimizer, epochs):
                 #   mask out invalid moves
                 invalidMoves = np.where( board.flatten() > 0, True, False)
                 maskedOutput = output.clone().view(9)
-                maskedOutput[invalidMoves] = 0
+                maskedOutput[invalidMoves] = -10
                 values, index = maskedOutput.max(0)
 
                 #   apply the move
@@ -269,7 +346,7 @@ def test(net, criterion, optimizer, epochs):
 
     return numWins, numLosses, numTies
 
-def playWithUser(net, criterion, optimizer, online=True):
+def playWithUser(net, online=True):
     while(True):
         board = genBoard()
         movesLeft = True
@@ -362,23 +439,13 @@ def playWithUser(net, criterion, optimizer, online=True):
 
 
 def main():
-    net = Net(18, 36, 9)
+    net = Net(18, 64, 9)
     # net = Net(18, 256, 9)
     criterion = nn.MSELoss(reduction='sum')
     optimizer = optim.Adam(net.parameters(), lr=0.001)
 
-    # print(net)
-
-    # train(net=net, epochs=100)
-
-    # board = genBoard()
-    # oneHot = oneHotTicTacToe(board=board, computersPlayer=2)
-    # oneHot = oneHot.reshape(1, 22)  #   presumably we could batch the inputs for rapid training
-
-    # output = net(oneHot)
-
-    train(net=net, criterion=criterion, optimizer=optimizer, epochs=1000)
-    numWins, numLosses, numTies = test(net=net, criterion=criterion, optimizer=optimizer, epochs=1000)
+    train(net=net, criterion=criterion, optimizer=optimizer, epochs=10000)
+    numWins, numLosses, numTies = test(net=net, epochs=1000)
     print("wins, losses, ties:")
     print(numWins)
     print(numLosses)
@@ -386,7 +453,6 @@ def main():
 
     # trainAgainstSelf(net=net, criterion=criterion, optimizer=optimizer, epochs=1000)
     # numWins, numLosses, numTies = test(net=net, criterion=criterion, optimizer=optimizer, epochs=1000)
-
     # print("wins, losses, ties:")
     # print(numWins)
     # print(numLosses)
